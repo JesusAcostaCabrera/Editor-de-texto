71c301a34b8cdec121895cc99e60b9bddd206259
Jesus Acosta <jesus_acosta05@hotmail.com>
2023-05-26 12:13:39 -0400

    Cambios UI + mas funcionalidades

--- Ui.py
+++ Ui.py
@@ -11,6 +11,7 @@ class MyGuI(QMainWindow):
         uic.loadUi('editor.ui', self) #Nombre de la interfaz creada
 
         #Variables "Globales" para el uso de botones
+        self.Path = ""
         self.font = "Arial"
         self.size = 12
         self.weight = 300
@@ -18,12 +19,12 @@ class MyGuI(QMainWindow):
         self.show()
 
         #Acciones de los botones
-        self.setWindowTitle("Tendencias Desarrollo de Software - Notepad")
+        self.setWindowTitle("Aplicación de notas")
 
         #File
-        self.actionOpen.triggered.connect(self.open_file)       #Abrir Archivo
-        self.actionSave.triggered.connect(self.save_file)       #Guardar Archivo
-        self.actionClose.triggered.connect(exit)                #Cerrar Archivo (Sin advertencias)
+        self.actionNew.triggered.connect(self.newFile)
+        self.actionOpen.triggered.connect(self.openFile)       #Abrir Archivo
+        self.actionSave.triggered.connect(self.saveFile)       #Guardar Archivo
 
         #######
 
@@ -38,34 +39,60 @@ class MyGuI(QMainWindow):
         #######
 
         #Appearance
-        self.action12pt.triggered.connect(lambda: self.change_size(12)) #Tamaño 12
-        self.action18pt.triggered.connect(lambda: self.change_size(18)) #Tamaño 18
-        self.action24pt.triggered.connect(lambda: self.change_size(24)) #Tamaño 24
-        self.actionBold.triggered.connect(lambda: self.set_bold())      #Estilo Negrita
-        self.actionItalic.triggered.connect(lambda: self.set_italic())  #Estilo Italica
-        self.actionReset.triggered.connect(lambda: self.reset_style())  #Reiniciar estilo
+        self.actionIncrease.triggered.connect(self.IncreaseFont) #Incrementar tamaño
+        self.actionDecrease.triggered.connect(self.DecreaseFont) #Disminuir tamaño
+        self.actionArial.triggered.connect(lambda: self.ChangeFont("Arial")) #Letra arial
+        self.actionTimes.triggered.connect(lambda: self.ChangeFont("Times New Roman")) #Letra times new roman
+        self.actionBold.triggered.connect(self.setBold)         #Estilo Negrita
+        self.actionItalic.triggered.connect(self.setItalic)     #Estilo Italica
+        self.actionReset.triggered.connect(self.resetStyle)     #Reiniciar estilo
+        self.actionDark.triggered.connect(self.setDarkMode)     #Modo Luz
+        self.actionLight.triggered.connect(self.setLightMode)   #Modo Oscuridad
 
 
     ##Metodos
 
     #File
 
-    
+    def newFile(self):
+        cancel = False
+        dialog = QMessageBox()
+        if self.plainTextEdit.toPlainText() != "":
+            dialog.setText("Do you want to save your work?")
+            dialog.addButton(QPushButton("Yes"), QMessageBox.YesRole) #0
+            dialog.addButton(QPushButton("No"), QMessageBox.NoRole) #1
+            dialog.addButton(QPushButton("Cancel"), QMessageBox.RejectRole) #2
+            answer = dialog.exec_()
+            if answer == 0:
+                self.saveFile()
+            if answer == 2:
+                cancel = True
+            self.Path = ""
+        if cancel == False:
+            self.plainTextEdit.clear()
+            self.resetStyle()
+            self.setWindowTitle("Untitled")
+
     #Abrir Archivo
-    def open_file(self):
+    def openFile(self):
         options = QFileDialog.Options()
-        filename, _ = QFileDialog.getOpenFileName(self, "OpenFile","", "TextFiles (*.txt);;Python Files (*.py)", options=options)
-        if filename != "":
-            with open(filename, "r") as f:
+        self.Path, _ = QFileDialog.getOpenFileName(self, "OpenFile","", "TextFiles (*.txt);;Python Files (*.py)", options=options)
+        if self.Path != "":
+            self.setWindowTitle(self.Path)
+            with open(self.Path, "r") as f:
                 self.plainTextEdit.setPlainText(f.read())
 
     #Guardar Archivo
-    def save_file(self):
-        options = QFileDialog.Options()
-        filename, _ = QFileDialog.getSaveFileName(self, "Save", "", "Text Files (*.txt);;All Files (*)", options=options)
-        if filename != "":
-            with open(filename, "w") as f:
-                f.write(self.plainTextEdit.toPlainText())
+    def saveFile(self):
+        if self.Path == "":
+            options = QFileDialog.Options()
+            self.Path, _ = QFileDialog.getSaveFileName(self, "Save", "", "Text Files (*.txt);;All Files (*)", options=options)
+            if self.Path != "":
+                with open(self.Path, "w") as f:
+                    f.write(self.plainTextEdit.toPlainText())
+        else:
+            with open(self.Path, "w") as f:
+                    f.write(self.plainTextEdit.toPlainText())
     
     #Cerrar con X
     def closeEvent(self, event):
@@ -98,7 +125,6 @@ class MyGuI(QMainWindow):
     def copy(self):
         self.plainTextEdit.copy()
 
-
     def paste(self):
         self.plainTextEdit.paste()
 
@@ -106,12 +132,22 @@ class MyGuI(QMainWindow):
 
     #Appearance
     #Cambiar tamaño de letras
-    def change_size(self, size):
-        self.plainTextEdit.setFont(QFont(self.font, size,self.weight))
-        self.size = size
+    def IncreaseFont(self):
+        self.size += 1
+        self.plainTextEdit.setFont(QFont(self.font, self.size, self.weight, self.italic))
+
+    def DecreaseFont(self):
+        self.size -= 1
+        self.plainTextEdit.setFont(QFont(self.font, self.size, self.weight, self.italic))
+    
+    #Cambiar tripos de letras
+
+    def ChangeFont(self, font):
+        self.font = font
+        self.plainTextEdit.setFont(QFont(self.font, self.size, self.weight, self.italic))
     
     #Cambiar texto a negrita
-    def set_bold(self):
+    def setBold(self):
 
         if self.weight != 500:
             self.weight = 500
@@ -121,7 +157,7 @@ class MyGuI(QMainWindow):
             self.plainTextEdit.setFont(QFont(self.font, self.size, self.weight, self.italic))
     
     #Cambiar texto a italico (?)
-    def set_italic(self):
+    def setItalic(self):
         
         if self.italic != True:
             self.italic = True
@@ -131,7 +167,43 @@ class MyGuI(QMainWindow):
             self.plainTextEdit.setFont(QFont(self.font,self.size,self.weight,self.italic))
 
     #Remover el estilo de negrita (e / o) italico 
-    def reset_style(self):
+    def resetStyle(self):
         self.plainTextEdit.setFont(QFont(self.font, self.size, 300, False))
         self.weight = 300
         self.italic = False
+
+    def setDarkMode(self):
+        self.setStyleSheet('''QWidget{
+                                    background-color : rgb(33,33,33);
+                                    color: #FFFFFF
+                                    }
+
+                                    QPlainTextEdit{
+                                    background-color : rgb(46,46,46)
+                                    }
+
+                                    QMenuBar::item:selected{
+                                    color: #000000
+                                    }
+
+                                    QMenu::item:selected{
+                                    background-color : rgb(46,46,46)
+                                    }
+                            ''')
+
+    def setLightMode(self):
+        self.setStyleSheet("")
+
+    def Check(self, event):
+        dialog = QMessageBox()
+        if self.plainTextEdit.toPlainText() != "":
+            dialog.setText("Do you want to save your work?")
+            dialog.addButton(QPushButton("Yes"), QMessageBox.YesRole) #0
+            dialog.addButton(QPushButton("No"), QMessageBox.NoRole) #1
+            dialog.addButton(QPushButton("Cancel"), QMessageBox.RejectRole) #2
+            answer = dialog.exec_()
+            if answer == 0:
+                self.save_file()
+                event.accept()
+            elif answer == 2:
+                event.ignore()
