32f9ea7174de25116f41dcd5223b6189f504f4b1
Jesus Acosta <jesus_acosta05@hotmail.com>
2023-05-26 10:48:40 -0400

    Copy, Paste, Cut, etc...

--- Ui.py
+++ Ui.py
@@ -19,6 +19,25 @@ class MyGuI(QMainWindow):
 
         #Acciones de los botones
         self.setWindowTitle("Tendencias Desarrollo de Software - Notepad")
+
+        #File
+        self.actionOpen.triggered.connect(self.open_file)       #Abrir Archivo
+        self.actionSave.triggered.connect(self.save_file)       #Guardar Archivo
+        self.actionClose.triggered.connect(exit)                #Cerrar Archivo (Sin advertencias)
+
+        #######
+
+        #Edit
+        self.actionUndo.triggered.connect(self.undo)
+        self.actionRedo.triggered.connect(self.redo)
+
+        self.actionCut.triggered.connect(self.cut)
+        self.actionCopy.triggered.connect(self.copy)
+        self.actionPaste.triggered.connect(self.paste)
+
+        #######
+
+        #Appearance
         self.action12pt.triggered.connect(lambda: self.change_size(12)) #Tamaño 12
         self.action18pt.triggered.connect(lambda: self.change_size(18)) #Tamaño 18
         self.action24pt.triggered.connect(lambda: self.change_size(24)) #Tamaño 24
@@ -26,41 +45,12 @@ class MyGuI(QMainWindow):
         self.actionItalic.triggered.connect(lambda: self.set_italic())  #Estilo Italica
         self.actionReset.triggered.connect(lambda: self.reset_style())  #Reiniciar estilo
 
-        self.actionOpen.triggered.connect(self.open_file)       #Abrir Archivo
-        self.actionSave.triggered.connect(self.save_file)       #Guardar Archivo
-        self.actionClose.triggered.connect(exit)                #Cerrar Archivo (Sin advertencias)
 
-    #Cambiar tamaño de letras
-    def change_size(self, size):
-        self.plainTextEdit.setFont(QFont(self.font, size,self.weight))
-        self.size = size
+    ##Metodos
+
+    #File
+
     
-    #Cambiar texto a negrita
-    def set_bold(self):
-
-        if self.weight != 500:
-            self.weight = 500
-            self.plainTextEdit.setFont(QFont(self.font, self.size, self.weight, self.italic))
-        else:
-            self.weight = 300
-            self.plainTextEdit.setFont(QFont(self.font, self.size, self.weight, self.italic))
-    
-    #Cambiar texto a italico (?)
-    def set_italic(self):
-        
-        if self.italic != True:
-            self.italic = True
-            self.plainTextEdit.setFont(QFont(self.font,self.size,self.weight,self.italic))
-        else:
-            self.italic = False
-            self.plainTextEdit.setFont(QFont(self.font,self.size,self.weight,self.italic))
-
-    #Remover el estilo de negrita (e / o) italico 
-    def reset_style(self):
-        self.plainTextEdit.setFont(QFont(self.font, self.size, 300, False))
-        self.weight = 300
-        self.italic = False
-
     #Abrir Archivo
     def open_file(self):
         options = QFileDialog.Options()
@@ -91,3 +81,57 @@ class MyGuI(QMainWindow):
                 event.accept()
             elif answer == 2:
                 event.ignore()
+
+    #######
+
+    #Edit
+
+    def undo(self):
+        self.plainTextEdit.undo()
+
+    def redo(self):
+        self.plainTextEdit.redo()
+
+    def cut(self):
+        self.plainTextEdit.cut()
+
+    def copy(self):
+        self.plainTextEdit.copy()
+
+
+    def paste(self):
+        self.plainTextEdit.paste()
+
+    #######
+
+    #Appearance
+    #Cambiar tamaño de letras
+    def change_size(self, size):
+        self.plainTextEdit.setFont(QFont(self.font, size,self.weight))
+        self.size = size
+    
+    #Cambiar texto a negrita
+    def set_bold(self):
+
+        if self.weight != 500:
+            self.weight = 500
+            self.plainTextEdit.setFont(QFont(self.font, self.size, self.weight, self.italic))
+        else:
+            self.weight = 300
+            self.plainTextEdit.setFont(QFont(self.font, self.size, self.weight, self.italic))
+    
+    #Cambiar texto a italico (?)
+    def set_italic(self):
+        
+        if self.italic != True:
+            self.italic = True
+            self.plainTextEdit.setFont(QFont(self.font,self.size,self.weight,self.italic))
+        else:
+            self.italic = False
+            self.plainTextEdit.setFont(QFont(self.font,self.size,self.weight,self.italic))
+
+    #Remover el estilo de negrita (e / o) italico 
+    def reset_style(self):
+        self.plainTextEdit.setFont(QFont(self.font, self.size, 300, False))
+        self.weight = 300
+        self.italic = False
