744e3befaa703cf92292fb246fe991e6e8e7334b
Jesus Acosta <jesus_acosta05@hotmail.com>
2023-06-07 19:08:43 -0400

    Set Variables ambiente

--- App Html/md.py
+++ App Html/md.py
@@ -17,32 +17,28 @@ from PyQt5.QtWebEngineWidgets import QWebEngineView
 import sys
 
 # Convertir el texto Markdown a HTML
-with open("Test.txt", "r", encoding="utf-8") as input_file:
-    text = input_file.read()
-html = markdown.markdown(text)
+def write_MD():
+    with open("Test.txt", "r", encoding="utf-8") as input_file:
+        text = input_file.read()
+        html = markdown.markdown(text)
 
-
-# Crear una aplicación de PyQt
+    with open("test.html", "w", encoding="utf-8", errors="xmlcharrefreplace") as output_file:
+        output_file.write(html)
 app = QApplication(sys.argv)
-
-# Crear una instancia de QWebEngineView
 view = QWebEngineView()
 
 # Cargar el archivo HTML
-file_path = r"D:\Proyectos de Programación\python\Markdown\test.html"  # Reemplaza con la ruta de tu archivo HTML
+file_path = r"D:\Proyectos de Programación\python\Notepad\App Html\test.html"
 view.load(QUrl.fromLocalFile(file_path))
 
-# Función para actualizar la vista
 def update_view():
+    write_MD()
     view.reload()
 
 # Configurar el temporizador para actualizar cada 5 segundos
 timer = QTimer()
 timer.timeout.connect(update_view)
-timer.start(5000)  # 5000 milisegundos = 5 segundos
-
-# Mostrar la ventana
+timer.start(5000)
 view.show()
 
-# Ejecutar el bucle de eventos de la aplicación
 sys.exit(app.exec_())
